c0810b405422f3b2b0b170f03664a624762e423d
Ian Bunnell <ibunnell@uci.edu>
2024-04-16 01:47:03 -0700

    edge detection shader

--- Assets/EdgeDetection.cs
+++ Assets/EdgeDetection.cs
@@ -6,8 +6,8 @@ using UnityEngine.Rendering.PostProcessing;
 [PostProcess(typeof(EdgeDetectionRenderer), PostProcessEvent.AfterStack, "Custom/EdgeDetection")]
 public sealed class EdgeDetection : PostProcessEffectSettings
 {
-    [Range(0f, 1f), Tooltip("Edge detection effect intensity.")]
-    public FloatParameter blend = new FloatParameter { value = 0.5f };
+    [Range(0f, 5f), Tooltip("Edge detection effect intensity.")]
+    public FloatParameter range = new FloatParameter { value = 1f };
 }
 
 public sealed class EdgeDetectionRenderer : PostProcessEffectRenderer<EdgeDetection>
@@ -15,7 +15,7 @@ public sealed class EdgeDetectionRenderer : PostProcessEffectRenderer<EdgeDetect
     public override void Render(PostProcessRenderContext context)
     {
         var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/EdgeDetection"));
-        sheet.properties.SetFloat("_Blend", settings.blend);
+        sheet.properties.SetFloat("_Range", settings.range);
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
--- Assets/EdgeDetection.shader
+++ Assets/EdgeDetection.shader
@@ -5,14 +5,27 @@ Shader "Hidden/Custom/EdgeDetection"
 #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
 
 		TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
-	float _Blend;
+	float _Range;
 
 	float4 Frag(VaryingsDefault i) : SV_Target
 	{
 		float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
-		float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
-		color.rgb = lerp(color.rgb, luminance.xxx, _Blend.xxx);
-		return color;
+		
+		float2 pixelRange = _Range * (_ScreenParams.zw - float2(1, 1));
+		float4 colorTotals;
+		for (int a = -1; a <= 1; a++)
+		{
+			for (int b = -1; b <= 1; b++)
+			{
+				if(a != 0 || b != 0)
+				{
+					colorTotals += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord + float2(pixelRange.x * a, pixelRange.y * b));
+				}
+			}
+		}
+		float4 colorAvg = colorTotals / 8;
+		float4 colorDiff = color - colorAvg;
+		return lerp(color, float4(0, 0, 0, 0), sqrt(sqrt(abs(colorDiff.r) + abs(colorDiff.g) + abs(colorDiff.b) + abs(colorDiff.a))));
 	}
 
 		ENDHLSL
